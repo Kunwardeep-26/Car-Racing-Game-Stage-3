e4b1df1d37b75c145ab66e262ed6948d89cdfa43
Kunwardeep-26 <kunwar1212juneja@gmail.com>
2021-01-14 17:40:29 +0530

    Displaying Rank

---
+++ README.md
@@ -0,0 +1,3 @@
+# Car Racing Game Stage 3
+Store rank of individual players in the game as player property.
+Display rank of the player when the game ends.
--- js/Player.js
+++ js/Player.js
@@ -3,6 +3,7 @@ class Player {
     this.index = null;
     this.distance = 0;
     this.name = null;
+    this.rank = null;
   }
 
   getCount(){
@@ -31,5 +32,44 @@ class Player {
     playerInfoRef.on("value",(data)=>{
       allPlayers = data.val();
     })
+
   }
+  getCarsAtEnd(){
+    database.ref('CarsAtEnd').on("value", (data)=>{
+      this.rank = data.val();
+    });
+
+  }
+
+  static updateCarsAtEnd(rank){
+    database.ref('/').update({
+      CarsAtEnd:rank 
+    })
+  }
+
+  /*display(){
+    if(gameState===2){
+      if(player1.distance>player2.distance && player1.distance>player3.distance
+        && player1.distance>player4.distance){
+          text("Player1 " + this.player.distance);
+          text("Player2 " + this.player.distance);
+          text("Player3 " + this.player.distance);
+          text("Player4 " + this.player.distance);
+        }
+      else if(player2.distance>player1.distance && player2.distance>player3.distance
+        && player2.distance>player4.distance){
+          text("Player2 " + this.player.distance);
+          text("Player1 " + this.player.distance);
+          text("Player3 " + this.player.distance);
+          text("Player4 " + this.player.distance);
+        }
+      else if(player3.distance>player1.distance && player3.distance>player2.distance
+        && player3.distance>player4.distance){
+          text("Player3 " + this.player.distance);
+          text("Player1 " + this.player.distance);
+          text("Player2 " + this.player.distance);
+          text("Player4 " + this.player.distance);          
+        }
+    }
+  }*/
 }
